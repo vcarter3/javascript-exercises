1e6a12f4435a1e196f58b6f1a6a93273e3afadd6
Veronica Carter <121vcarter@gmail.com>
2022-07-06 09:06:19 +0100

    completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function (string, number) {
+    let modified_string = string;
+    if (number == 0) {
+        return ""
+    }
 
+    if (number < 0) {
+        return "ERROR"
+    }
+
+    for (i = 1; i < number; i++) {
+        modified_string += string;
+    }
+
+    return modified_string
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let string_array = string.split('');
+    let reversed_array = string_array.reverse();
+    let reversed_string = reversed_array.join('');
 
+    return reversed_string
 };
 
 // Do not edit below this line
