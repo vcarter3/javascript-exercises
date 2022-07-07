e3dc6e7eb12901652032bdf9ab31af27d9cf8f4e
vcarter3 <121vcarter@gmail.com>
2022-07-07 09:02:35 +0100

    completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+    if (index < 0) { return "OOPS" }
 
+    function generator(n) {
+        if (n == 0) { return 0 };
+        if (n == 1) { return 1 };
+        return generator(n - 1) + generator(n - 2)
+    }
+
+    return generator(parseInt(index))
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = []
+    for(let i =0; i<books.length; i++){
+        titles.push(books[i].title)
+    }
+    return titles
 };
 
 // Do not edit below this line
