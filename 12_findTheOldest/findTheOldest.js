0e8d789f00e2c54552db9ab13411f96e5c22de98
Veronica Carter <121vcarter@gmail.com>
2022-07-07 10:51:07 +0100

    completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (oldies) {
+    age_max = 0
+    person_index = 0
+
+    for (i = 0; i < oldies.length; i++) {
+        if (Object.hasOwn(oldies[i], 'yearOfDeath')) {
+            deathYear = oldies[i].yearOfDeath;
+        } else {
+            deathYear = new Date().getFullYear();
+        }
+
+
+        if (deathYear - oldies[i].yearOfBirth > age_max) {
+            age_max = deathYear - oldies[i].yearOfBirth;
+            person_index = i;
+        }
+    }
+    return oldies[person_index]
 
 };
 
